31a03615fcf7c7555d03bfda02d4c7c9c4a8aaee
Raj Shakya <119093677+Rajshakya0101@users.noreply.github.com>
2024-12-02 15:44:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pulp
 import logging
 import sys
 import re
+import os
 
 app = Flask(__name__)
 
@@ -117,5 +118,8 @@ def solve_task_scheduler():
         return jsonify({"error": str(e)}), 400
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
